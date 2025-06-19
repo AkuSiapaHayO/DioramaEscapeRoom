0bec211019e9f517cef32007617caefd619b298c
derendmarvel <derend101@gmail.com>
2025-06-19 23:59:58 +0700

    Home & Levels UI

---
+++ DioramaEscapeRoom/Utility/TransparentSceneView.swift
@@ -0,0 +1,27 @@
+//
+//  TransparentSceneView.swift
+//  DioramaEscapeRoom
+//
+//  Created by Derend Marvel Hanson Prionggo on 19/06/25.
+//
+
+import Foundation
+import SwiftUI
+import SceneKit
+
+struct TransparentSceneView: UIViewRepresentable {
+    let scene: SCNScene
+
+    func makeUIView(context: Context) -> SCNView {
+        let view = SCNView()
+        view.scene = scene
+        view.allowsCameraControl = false
+        view.backgroundColor = .clear    // ✅ Transparent
+        view.isOpaque = false            // ✅ Required for transparency
+        return view
+    }
+
+    func updateUIView(_ uiView: SCNView, context: Context) {
+        // Optional: Update logic if needed
+    }
+}
