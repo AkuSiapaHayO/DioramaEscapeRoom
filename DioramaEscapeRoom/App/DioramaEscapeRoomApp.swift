5e9a95ea8c088cc60775e30327ba4c49d8b850ce
Louis Mario <126391560+AkuSiapaHayO@users.noreply.github.com>
2025-06-20 08:45:52 +0700

    Launch Screen

--- DioramaEscapeRoom/App/DioramaEscapeRoomApp.swift
+++ DioramaEscapeRoom/App/DioramaEscapeRoomApp.swift
@@ -10,15 +10,32 @@ import SwiftUI
 @main
 struct DioramaEscapeRoomApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @State private var showLaunchScreen = true
 
     var body: some Scene {
         WindowGroup {
-            MainMenuView()
-                .onAppear {
-                    UIDevice.current.setValue(UIInterfaceOrientation.landscapeLeft.rawValue, forKey: "orientation")
-                    AppDelegate.orientationLock = .landscape
+            ZStack {
+                // MainMenu fades in
+                MainMenuView()
+                    .opacity(showLaunchScreen ? 0 : 1)
+                    .animation(.easeInOut(duration: 2), value: showLaunchScreen)
+
+                // LaunchScreen fades out
+                if showLaunchScreen {
+                    LaunchScreen()
+                        .transition(.opacity)
+                        .animation(.easeInOut(duration: 2), value: showLaunchScreen)
                 }
+            }
+            .onAppear {
+                UIDevice.current.setValue(UIInterfaceOrientation.landscapeLeft.rawValue, forKey: "orientation")
+                AppDelegate.orientationLock = .landscape
+
+                // Force 3-second display of launch screen
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    showLaunchScreen = false
+                }
+            }
         }
     }
 }
-
